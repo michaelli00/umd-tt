5e88da96b14c784e09dbb2f3ffe241df265d63aa
Yash Kapoor <yash.kapa4@gmail.com>
2022-05-28 23:00:27 -0700

    adding some comments and commands for inserting test values to server/database.sql

--- server/database.sql
+++ server/database.sql
@@ -1,3 +1,7 @@
+-- i have my instance of postgres set up with both username and password being postgres
+-- so to interact with the database i use the following commands
+-- psql -U postgres league     <-- league is the name of the database and should only be specified from the second time around, after you have created it using the command below
+-- the first time u enter psql and create league you can exit and use the above command to get into it
 -- creating the database called league
 create database league;
 
@@ -26,4 +30,11 @@ create table matches (
 
 
 -- inserting some values for testing
-insert into ratings (player_name, rating, active) values ('yash', 2400, TRUE), ('michael', 300, TRUE), ('chris', 400, TRUE);
+insert into ratings (player_name, rating, active) values 
+    ('yash', 2400, TRUE), ('michael', 300, TRUE), ('chris', 400, FALSE), ('jamie', 1600, TRUE);
+
+insert into events (event_name,event_date) values
+    ('test1','2022-01-01'), ('test2','2022-03-03');
+
+insert into matches values 
+    (1,1,2,3,0,2), (1,1,3,3,0,5), (1,2,3,4,1,9), (1,4,1,3,2,8);
